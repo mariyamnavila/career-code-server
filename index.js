bde7675a5ed6b5cc64d78cefdba5352b9ec2b03e
mariyamnavila <bibimariyamnavila@gmail.com>
2025-11-05 21:38:42 +0600

    added insertOne for job

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/jobs', async (req, res) => {
+            const newJob = req.body;
+            const result = await jobsCollection.insertOne(newJob);
+            res.send(result)
+        })
+
         // job applications
 
         app.get('/applications', async (req, res) => {
