e4d202b1117fb6b60e81d122fe1bdb19076c56c4
mariyamnavila <bibimariyamnavila@gmail.com>
2025-11-09 16:49:48 +0600

    added find for jobs/applications

--- index.js
+++ index.js
@@ -44,6 +44,19 @@ async function run() {
         //     const result = await jobsCollection.find(query).toArray();
         //     res.send(result)
         // })
+        app.get('/jobs/applications', async (req, res) => {
+            const email = req.query.email;
+            const query = { hr_email: email };
+            const jobs = await jobsCollection.find(query).toArray();
+
+            // should use aggregate to have optimum data fetching
+            for (const job of jobs) {
+                const applicationQuery = { jobId: job._id.toString() }
+                const application_count = await applicationsCollection.countDocuments(applicationQuery)
+                job.application.count = application_count;
+            }
+            res.send(jobs)
+        })
 
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
@@ -57,6 +70,7 @@ async function run() {
             const result = await jobsCollection.insertOne(newJob);
             res.send(result)
         })
+
 
         // job applications
 
